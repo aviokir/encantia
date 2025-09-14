081125a27b7af38ad119da1038faa4427c63ace8
MrAlexXD_ <andreeavioletak@gmail.com>
2025-09-15 01:19:33 +0200

    Delete components directory

--- components/MusicToastWrapper.js
+++
@@ -1,7 +0,0 @@
-import MusicToast from './MusicToast';
-import { useMusic } from '../context/MusicContext';
-
-export default function MusicToastWrapper() {
-    const { currentMusic, isPlaying } = useMusic();
-    return <MusicToast currentMusic={currentMusic} isPlaying={isPlaying} />;
-}
