6fa54f7d4f0bcb8d7e20e9825be55b41ee88cfc3
MrAlexXD_ <andreeavioletak@gmail.com>
2025-09-18 00:14:50 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'custom', // Usamos un loader personalizado
+    domains: ['storage.encantia.online'], // Tu dominio personalizado
+  },
 };
 
 export default nextConfig;
