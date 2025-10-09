1664283bee97c4bd87b2135f83b1e5b5b2d678bf
MrAlexXD_ <andreeavioletak@gmail.com>
2025-10-09 22:41:50 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,14 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
+// /pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="es">
       <Head>
         {/* Script de Google AdSense */}
         <script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8562146544458621"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5967987746854372"
           crossOrigin="anonymous"
         ></script>
       </Head>
