672ccb4c75521c2c98497766827142f0326fb87c
alexmfa011-boop <alexmfa011@gmail.com>
2026-01-12 21:59:38 +0100

    Delete pages directory

--- pages/api/oauth/spotify.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { user_id } = req.query;
-  const clientId = process.env.SPOTIFY_CLIENT_ID;
-  const redirectUri = `${process.env.NEXT_PUBLIC_URL}/api/oauth/callback/spotify`;
-  const scopes = "user-read-playback-state user-read-currently-playing";
-
-  const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
-  res.redirect(url);
-}
